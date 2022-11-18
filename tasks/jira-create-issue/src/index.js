70c9fd2f6e4d4313881f6db59e02aa06e9f4c95c
Abhishek Kumar Singh <abhishekk@jfrog.com>
2022-11-18 16:32:26 +0530

    Adds create-jira-issue task

---
+++ tasks/jira-create-issue/src/index.js
@@ -0,0 +1,40 @@
+const tasks = require("jfrog-pipelines-tasks");
+const axios = require("axios");
+
+async function main() {
+  const summary = tasks.getInput("summary");
+  const projectKey = tasks.getInput("projectKey");
+  const issueTypeId = tasks.getInput("issueTypeId");
+  const description = tasks.getInput("description");
+  const username = tasks.getInput("username");
+  const token = tasks.getInput("token");
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    auth: {
+      username: username,
+      password: token,
+    },
+    data: {
+      fields: {
+        summary,
+        project: {
+          key: projectKey,
+        },
+        issuetype: {
+          id: issueTypeId,
+        },
+        description,
+      },
+    },
+    url: endpoint + "/rest/api/2/issue",
+  };
+  await axios(options);
+}
+
+(async () => {
+  await main();
+})();
